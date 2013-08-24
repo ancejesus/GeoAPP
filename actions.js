c638b0e7ed7a412925ada73c97a61de704583222
Jesus AnCe <jandrade@curves.com>
2013-08-24 12:38:19 -0500

    Ver 1.0
    Geolocalizacion Actual usando geolocation.getCurrentPosition

---
+++ actions.js
@@ -0,0 +1,32 @@
+// actions.js {}
+$(document).ready(function(e) {
+	document.addEventListener("deviceready",function(){
+	var onSuccess = function(position) {
+    initialize(position.coords.latitude,position.coords.longitude);
+};
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+},false);	
+});
+
+
+function initialize(lat,long) {
+				//Posición del mapa
+				var latlng = new google.maps.LatLng(lat,long);
+				var myOptions = {
+					zoom: 5,
+					center: latlng,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				};
+				var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+				//Marcador
+				var marker = new google.maps.Marker({
+					position: latlng, 
+					map: map, 
+					title:"OperadoraFQS"
+				});
+			}
